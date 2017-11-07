7f287c4fa94e4af54856df82df85844ed25540a7
Baboo7 <baptiste.studer@laposte.net>
2017-11-07 15:55:15 -0800

    remove use of rxjs subject in dialogflow service

--- src/app/_services/dialogflow.service.ts
+++ src/app/_services/dialogflow.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { Subject } from 'rxjs/Subject';
-import { Subscription } from 'rxjs/Subscription';
+import { Observable } from 'rxjs/Observable';
 
 import { environment } from '../../environments/environment';
 import { UuidService } from './uuid.service'
@@ -9,22 +8,9 @@ import { UuidService } from './uuid.service'
 @Injectable()
 export class DialogFlowService {
 
-  private subject = new Subject();
   private sessionId = this.uuidService.generateUuid();
 
   constructor(private http: HttpClient, private uuidService: UuidService) { }
-
-  /*  Subscribe to the answers of the agent.
-
-  PARAMS
-    observer (function|observer)
-
-  RETURN
-    subscription object
-  */
-  public subscribe(observer): Subscription {
-    return this.subject.subscribe(observer);
-  }
 
   /*  Send a message to the agent.
 
@@ -34,22 +20,26 @@ export class DialogFlowService {
   RETURN
     none
   */
-  public sendMessage(query: string): void {
-    let url = `${environment.dialogflow.queryUrl}?v=${environment.dialogflow.v}`;
+  public sendMessage(query: string): Observable<{}> {
+    let observable = new Observable(observer => {
+      let url = `${environment.dialogflow.queryUrl}?v=${environment.dialogflow.v}`;
 
-    let headers = new HttpHeaders()
-    .set('Authorization', `Bearer ${environment.dialogflow.devKey}`)
-    .set('Content-Type', 'application/json; charset=utf-8');
+      let headers = new HttpHeaders()
+      .set('Authorization', `Bearer ${environment.dialogflow.devKey}`)
+      .set('Content-Type', 'application/json; charset=utf-8');
 
-    let body = {
-      sessionId: this.sessionId,
-      lang: 'en',
-      query: query
-    };
+      let body = {
+        sessionId: this.sessionId,
+        lang: 'en',
+        query: query
+      };
 
-    this.http.post(url, body, { headers })
-    .subscribe(raw => {
-      this.subject.next(raw);
+      this.http.post(url, body, { headers })
+      .subscribe(raw => {
+        observer.next(raw);
+      });
     });
+
+    return observable;
   }
 }
