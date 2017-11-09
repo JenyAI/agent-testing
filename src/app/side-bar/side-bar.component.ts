2e9d1a407b54aa00d7d4a8a1ba2aa517c303f46c
Baboo7 <baptiste.studer@laposte.net>
2017-11-08 16:15:29 -0800

    clean side bar component

--- src/app/side-bar/side-bar.component.ts
+++ src/app/side-bar/side-bar.component.ts
@@ -1,15 +1,8 @@
 import { Component } from '@angular/core';
 
-import { SituationService } from '../_services/situation.service';
-
 @Component({
   selector: 'app-side-bar',
   templateUrl: './side-bar.component.html',
   styleUrls: [ './side-bar.component.scss' ]
 })
-export class SideBarComponent {
-
-  private situations: any[ ] = this.situationService.getSituations();
-
-  constructor(private situationService: SituationService) { }
-}
+export class SideBarComponent { }
