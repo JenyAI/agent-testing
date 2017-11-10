81613e7607f96f2fe7c1562ef564bb0312b9e1a5
Baboo7 <baptiste.studer@laposte.net>
2017-11-09 18:43:39 -0800

    connect situation service to websocket server

--- src/app/_services/situation.service.ts
+++ src/app/_services/situation.service.ts
@@ -3,15 +3,132 @@ import { Observable } from 'rxjs/Observable';
 import { BehaviorSubject } from 'rxjs/BehaviorSubject';
 import { Subscription } from 'rxjs/Subscription';
 
+import { WebsocketService } from './websocket.service';
+
 @Injectable()
 export class SituationService {
-
-  private id: number = 0;
 
   private selectedIntentName: string = '';
 
   private situations: any[ ] = [ ];
   private subjectSituation: BehaviorSubject<any[]> = new BehaviorSubject([ ]);
+
+  constructor(private websocketService: WebsocketService) {
+    this.websocketService.addListener('situation-created').subscribe((situation: any) => this.onSituationCreated(situation));
+
+    this.websocketService.addListener('situation-deleted').subscribe((id: string) => this.onSituationDeleted(id));
+
+    this.websocketService.addListener('situation-updated').subscribe((situation: any) => this.onSituationUpdated(situation));
+
+    this.websocketService.addListener('situation-got-all').subscribe((situations: any[ ]) => this.onSituationsGot(situations));
+
+    this.websocketService.send('situation-get-all');
+  }
+
+  /*  Triggered when situation created on server.
+
+    PARAMS
+      situation (object)
+
+    RETURN
+      none
+  */
+  private onSituationCreated(situation: any): void {
+
+    this.situations.push(situation);
+    this.subjectSituation.next(this.situations);
+  }
+
+  /*  Triggered when situation deleted on server.
+
+    PARAMS
+      id (string): id of the deleted situation
+
+    RETURN
+      none
+  */
+  private onSituationDeleted(id: string): void {
+
+    this.situations = this.situations.filter(s => s.id != id);
+    this.subjectSituation.next(this.situations);
+  }
+
+  /*  Triggered when situation updated on server.
+
+    PARAMS
+      situation (object)
+
+    RETURN
+      none
+  */
+  private onSituationUpdated(situation: any): void {
+
+    let oldSituation = this.situations.find(s => s.id == situation.id);
+    oldSituation = situation;
+    this.subjectSituation.next(this.situations);
+  }
+
+  /*  Triggered when situation retrieved from server.
+
+    PARAMS
+      situations (array of objects)
+
+    RETURN
+      none
+  */
+  private onSituationsGot(situations: any[ ]): void {
+
+    this.situations = situations;
+    this.subjectSituation.next(this.situations);
+  }
+
+  /*  Create a situation
+
+    PARAMS
+      intentName (string)
+
+    RETURN
+      none
+  */
+  public createSituation(intentName: string = '', utterance: string = ''): void {
+
+    let situation = {
+      intentName: intentName,
+      utterance: utterance
+    };
+
+    this.websocketService.send('situation-create', situation);
+  }
+
+  /*  Delete a situation
+
+    PARAMS
+      id (number)
+
+    RETURN
+      none
+  */
+  public deleteSituation(id): void {
+
+    let data = {
+      id: id
+    };
+
+    this.websocketService.send('situation-delete', data);
+  }
+
+  /*  Update a situation
+
+    PARAMS
+      situation (object)
+
+    RETURN
+      none
+  */
+  public updateSituation(situation): void {
+
+    this.websocketService.send('situation-update', situation);
+  }
 
   /*  Subscribe to changes in the list of situations
 
@@ -23,27 +140,6 @@ export class SituationService {
   */
   public subscribeToSituations(observer): Subscription {
     return this.subjectSituation.subscribe(observer);
-  }
-
-  /*  Create a situation
-
-    PARAMS
-      intentName (string)
-
-    RETURN
-      none
-  */
-  public createSituation(intentName: string, utterance: string = ''): void {
-    let situation = {
-      id: this.id,
-      intentName: intentName,
-      utterance: utterance
-    };
-
-    this.id++;
-
-    this.situations.push(situation);
-    this.subjectSituation.next(this.situations);
   }
 
   /*  Get the selected intent name.
@@ -68,34 +164,5 @@ export class SituationService {
   */
   public updateSelectedIntentName(intentName): void {
     this.selectedIntentName = intentName;
-  }
-
-  /*  Update a situation
-
-    PARAMS
-      situation (object)
-
-    RETURN
-      none
-  */
-  public updateSituation(situation): void {
-    let oldSituation = this.situations.find(s => s.id == situation.id);
-    oldSituation = situation;
-    this.subjectSituation.next(this.situations);
-  }
-
-  /*  Delete a situation
-
-    PARAMS
-      id (number)
-
-    RETURN
-      none
-  */
-  public deleteSituation(id): void {
-    this.situations = this.situations.filter(s => {
-      return s.id != id;
-    });
-    this.subjectSituation.next(this.situations);
   }
 }
